4a361e3c0149fdbca11701c726587b252a28517e
Cesar Glufke Zanellato <cesar_zanellato@estudante.sesisenai.org.br>
2022-11-21 14:50:24 -0300

    atualização da página de compra 21/11 pt1

---
+++ Manto Rei S.A/Página de Compra/compra.js
@@ -0,0 +1,79 @@
+let alemanha = document.getElementById("link-alemanha")
+let argentina = document.getElementById("link-argentina")
+let brasil = document.getElementById("link-brasil")
+let coreia = document.getElementById("link-coreia")
+let franca = document.getElementById("link-franca")
+let holanda = document.getElementById("link-holanda")
+let portugal = document.getElementById("link-portugal")
+
+let barcelona = document.getElementById("link-barcelona")
+let borussia = document.getElementById("link-borussia")
+let liverpool = document.getElementById("link-liverpool")
+let mancity = document.getElementById("link-mancity")
+let milan = document.getElementById("link-milan")
+let psg = document.getElementById("link-psg")
+let realmadrid = document.getElementById("link-realmadrid")
+
+let corinthias = document.getElementById("link-corinthias")
+let flamengo = document.getElementById("link-flamengo")
+let fluminense = document.getElementById("link-fluminense")
+let gremio = document.getElementById("link-gremio")
+let internacial = document.getElementById("link-internacial")
+let palmeiras = document.getElementById("link-palmeiras")
+let santos = document.getElementById("link-santos")
+
+
+let alemanhaImgIndex = 0
+let argentinaImgIndex = 0
+let brasilImgIndex = 0
+let coreiaImgIndex = 0
+let francaImgIndex = 0
+let holandaImgIndex = 0
+let portugalImgIndex = 0
+
+let ImgIndex = 0
+let ImgIndex = 0
+let ImgIndex = 0
+let ImgIndex = 0
+let ImgIndex = 0
+let ImgIndex = 0
+let ImgIndex = 0
+
+let ImgIndex = 0
+let ImgIndex = 0
+let ImgIndex = 0
+let ImgIndex = 0
+let ImgIndex = 0
+let ImgIndex = 0
+let ImgIndex = 0
+
+function Alemanha1(){
+
+    switch(playerImgIndex){
+    
+
+        case 1:
+
+            alemanha.src = "../IMG/camisaAlemanha1.png"
+            playerImgIndex = 1
+            break
+
+        case 2:
+
+            alemanha.src = "../IMG/camisaAlemanha2.png"
+            playerImgIndex = 2
+            break
+
+        case 3:
+
+            alemanha.src = "../IMG/camisaAlemanha3.png"
+            playerImgIndex = 3
+            break
+
+    }
+
+}
+
+
+
+}
